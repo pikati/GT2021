a53e38938dc31a09fcaa4f5aadb2b48c49230ba1
pikatan <pikapisu@gmail.com>
2021-04-19 17:48:54 +0900

    テスト

--- Prototype/Assets/Pikatan/Scripts/Test2/PointSetter.cs
+++ Prototype/Assets/Pikatan/Scripts/Test2/PointSetter.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PointSetter : MonoBehaviour
 {
     public MovePoint MovePoint { get; private set; }
-    // Start is called before the first frame update
     void Awake()
     {
         MovePoint = new MovePoint();
