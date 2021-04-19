3fa175b5da60889c3868483aa4af77250160f287
pikatan <pikapisu@gmail.com>
2021-04-19 12:05:39 +0900

    初コミット

---
+++ Prototype/Assets/NavMeshTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[DefaultExecutionOrder(-103)]
+public class NavMeshTest : MonoBehaviour
+{
+    NavMeshSurface surface;
+    void Start()
+    {
+        surface = GetComponent<NavMeshSurface>();
+        surface.BuildNavMesh();
+        StartCoroutine(TimeUpdate());
+    }
+
+    IEnumerator TimeUpdate()
+    {
+        while (true)
+        {
+            surface.BuildNavMesh();
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
---
+++ Prototype/Assets/TargetSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSelector : MonoBehaviour
+{
+    private GameObject[] targetObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetObjects = GameObject.FindGameObjectsWithTag("Panel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 SelectTargetPosition()
+    {
+        return Vector3.zero;
+    }
+}
