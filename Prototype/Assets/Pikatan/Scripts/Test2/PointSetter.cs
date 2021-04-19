e6d47b0cfa1c2c417742fee5f1c9a947aa1e5ed8
pikatan <pikapisu@gmail.com>
2021-04-19 17:30:53 +0900

    自分の作業用フォルダ作ったよ

---
+++ Prototype/Assets/Pikatan/Scripts/Test2/PointSetter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSetter : MonoBehaviour
+{
+    public MovePoint MovePoint { get; private set; }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MovePoint = new MovePoint();
+        MovePoint.SetPoint(gameObject.transform.position);
+    }
+}
