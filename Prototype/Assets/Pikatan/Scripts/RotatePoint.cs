1d5cebda1a298805f20484a72faf58fab32cd567
pikatan <pikapisu@gmail.com>
2021-04-20 23:43:16 +0900

    パネル回転機能実装完了

--- Prototype/Assets/Pikatan/Scripts/RotatePoint.cs
+++ Prototype/Assets/Pikatan/Scripts/RotatePoint.cs
@@ -24,7 +24,9 @@ public class RotatePoint : MonoBehaviour
     private RotateAxis rotateAxis = RotateAxis.Z; 
     private RotateState rotateState = RotateState.NoRotate;
     private bool isRotate = false;
-    private Vector3 rotateValue = Vector3.zero;
+    //private Vector3 rotateValue = Vector3.zero;
+    private float rotateValue;
+    private float[] angles = new float[3];
 
     private void Start()
     {
@@ -43,30 +45,15 @@ public class RotatePoint : MonoBehaviour
         if (isRotate)
         {
             float deg = rotateState == RotateState.NoRotate ? 180.0f : 0;
-            transform.Rotate(rotateValue * rotateSpeed * Time.deltaTime);
-            float angle = 0;
-            switch (rotateAxis)
+            //transform.Rotate(rotateValue * rotateSpeed * Time.deltaTime);
+            Debug.Log(rotateValue);
+            angles[(int)rotateAxis] += rotateValue * rotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.AngleAxis(angles[(int)rotateAxis], GetAxis());
+            if (IsRotateComplete(angles[(int)rotateAxis], deg))
             {
-                case RotateAxis.X:
-                    angle = transform.rotation.eulerAngles.x;
-                    break;
-                case RotateAxis.Y:
-                    angle = transform.rotation.eulerAngles.y;
-                    break;
-                case RotateAxis.Z:
-                    angle = transform.rotation.eulerAngles.z;
-                    break;
-                default:
-                    Debug.LogError("回転軸おかしくてワロタ");
-                    break;
-            }
-
-            if (IsRotateComplete(angle, deg))
-            {
-                Vector3 newAngle = SetNewAngle(deg);
-                transform.eulerAngles = newAngle;
+                angles[(int)rotateAxis] = deg;
                 isRotate = false;
-                if(rotateState == RotateState.NoRotate)
+                if (rotateState == RotateState.NoRotate)
                 {
                     rotateState = RotateState.Rotated;
                 }
@@ -81,21 +68,22 @@ public class RotatePoint : MonoBehaviour
     //初期化と回転フラグ立った時に呼べ
     private void SetRotateValue()
     {
-        switch (rotateAxis)
-        {
-            case RotateAxis.X:
-                rotateValue = rotateState == RotateState.NoRotate ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0);
-                break;
-            case RotateAxis.Y:
-                rotateValue = rotateState == RotateState.NoRotate ? new Vector3(0, 1, 0) : new Vector3(0, -1, 0);
-                break;
-            case RotateAxis.Z:
-                rotateValue = rotateState == RotateState.NoRotate ? new Vector3(0, 0, 1) : new Vector3(0, 0, -1);
-                break;
-            default:
-                Debug.LogError("回転軸おかしくてワロタ");
-                break;
-        }
+        //switch (rotateAxis)
+        //{
+        //    case RotateAxis.X:
+        //        rotateValue = rotateState == RotateState.NoRotate ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0);
+        //        break;
+        //    case RotateAxis.Y:
+        //        rotateValue = rotateState == RotateState.NoRotate ? new Vector3(0, 1, 0) : new Vector3(0, -1, 0);
+        //        break;
+        //    case RotateAxis.Z:
+        //        rotateValue = rotateState == RotateState.NoRotate ? new Vector3(0, 0, 1) : new Vector3(0, 0, -1);
+        //        break;
+        //    default:
+        //        Debug.LogError("回転軸おかしくてワロタ");
+        //        break;
+        //}
+        rotateValue = rotateState == RotateState.NoRotate ? 1 : -1;
     }
 
     private bool IsRotateComplete(float angle, float deg)
@@ -125,7 +113,7 @@ public class RotatePoint : MonoBehaviour
 
     private Vector3 SetNewAngle(float deg)
     {
-        Vector3 rot = transform.rotation.eulerAngles;
+        Vector3 rot = transform.localRotation.eulerAngles;
         switch (rotateAxis)
         {
             case RotateAxis.X:
@@ -134,6 +122,23 @@ public class RotatePoint : MonoBehaviour
                 return new Vector3(rot.x, deg, rot.z);
             case RotateAxis.Z:
                 return new Vector3(rot.x, rot.y, deg);
+            default:
+                Debug.LogError("回転軸おかしくてワロタ");
+                break;
+        }
+        return Vector3.zero;
+    }
+
+    private Vector3 GetAxis()
+    {
+        switch (rotateAxis)
+        {
+            case RotateAxis.X:
+                return Vector3.right;
+            case RotateAxis.Y:
+                return Vector3.up;
+            case RotateAxis.Z:
+                return Vector3.forward;
             default:
                 Debug.LogError("回転軸おかしくてワロタ");
                 break;
