5c5ee180a50150b4935e45329b5b031a4b054bca
Johnjohn55 <a144243495@icloud.com>
2021-04-21 17:35:59 +0900

    テクスチャいじった

---
+++ Prototype/Assets/ExplosionSsript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    //ParticleSystem型の変数
+    ParticleSystem ps;
+    //GameObject型の変数
+    GameObject obj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //FindメソッドでExplosionのGameObjectにアクセスして変数objで参照
+        obj = GameObject.Find("Explosion");
+        //GetComponentInChildrenで子要素も含めたParticleSystemコンポーネントにアクセスして変数㎰で参照
+        ps = obj.GetComponentInChildren<ParticleSystem>();
+        //変数objを非表示にしてParticleSystemの実行を止めておく
+        obj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左クリックされたら爆発のParticleSystemを実行する
+        if (Input.GetMouseButtonDown(0))
+        {
+            obj.SetActive(true);
+            ps.Play();
+        }
+
+    }
+}
