498b5b74b9077d9fca277d365167fe68d062b4ed
yamapiro36 <yukkuri0408@gmail.com>
2021-04-22 12:35:51 +0900

    色変わる処理

---
+++ Prototype/Assets/Yamapiro/Script/ChangeColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    bool GetFlag { get; set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetFlag == true)
+        {
+            //色を赤に変更
+            GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("選択されているよ");
+        }
+        else if (GetFlag == false)
+        {
+            Debug.Log("選択されてないよ");
+            //色を白に変更
+            GetComponent<Renderer>().material.color = Color.white;
+
+        }
+    }
+
+}
