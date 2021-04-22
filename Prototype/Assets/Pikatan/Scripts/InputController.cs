ff2aadff398ed43d830fcdcece24cb9d4e90b4ae
pikatan <pikapisu@gmail.com>
2021-04-22 16:43:07 +0900

    入力キーのバインドをしました

--- Prototype/Assets/Pikatan/Scripts/InputController.cs
+++ Prototype/Assets/Pikatan/Scripts/InputController.cs
@@ -7,13 +7,32 @@ public class InputController : Singleton<InputController>
 {
     #region Field
     private PlayerInput input;
-    private InputAction move, look;
-    private Vector2 moveValue, rightValue;
+    private InputAction move, look, a, b, x, y, rb, rt, r3, lb, lt, l3, select, start, up, down, left, right;
+    private Vector2 moveValue, rightValue, arrowValue;
     #endregion
 
     #region Property
     public Vector2 MoveValue => moveValue;
     public Vector2 RightStickValue => rightValue;
+    public Vector2 ArrowValue => arrowValue;
+    public bool A { get; private set; }
+    public bool B { get; private set; }
+    public bool X { get; private set; }
+    public bool Y { get; private set; }
+    public bool RB { get; private set; }
+    public bool RT { get; private set; }
+    public bool R3 { get; private set; }
+    public bool LB { get; private set; }
+    public bool LT { get; private set; }
+    public bool L3 { get; private set; }
+    public bool SelectPress { get; private set; }
+    public bool StartPress { get; private set; }
+    public bool Up { get; private set; }
+    public bool Down { get; private set; }
+    public bool Left { get; private set; }
+    public bool Right { get; private set; }
+
+
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -22,6 +41,22 @@ public class InputController : Singleton<InputController>
         var actionMap = input.currentActionMap;
         move = actionMap["Move"];
         look = actionMap["Look"];
+        a = actionMap["A"];
+        b = actionMap["B"];
+        x = actionMap["X"];
+        y = actionMap["Y"];
+        lb = actionMap["LB"];
+        lt = actionMap["LT"];
+        l3 = actionMap["L3"];
+        rb = actionMap["RB"];
+        rt = actionMap["RT"];
+        r3 = actionMap["R3"];
+        select = actionMap["Select"];
+        start = actionMap["Start"];
+        up = actionMap["Up"];
+        down = actionMap["Down"];
+        left = actionMap["Left"];
+        right = actionMap["Right"];
     }
 
     // Update is called once per frame
@@ -29,5 +64,25 @@ public class InputController : Singleton<InputController>
     {
         moveValue = move.ReadValue<Vector2>();
         rightValue = look.ReadValue<Vector2>();
+        A = a.triggered;
+        B = b.triggered;
+        X = x.triggered;
+        Y = y.triggered;
+        RB = rb.triggered;
+        RT = rt.triggered;
+        R3 = r3.triggered;
+        LB = lb.triggered;
+        LT = lt.triggered;
+        L3 = l3.triggered;
+        SelectPress = select.triggered;
+        StartPress = start.triggered;
+        Up = up.triggered;
+        Down = down.triggered;
+        Left = left.triggered;
+        Right = right.triggered;
+        Debug.Log("up:" + Up);
+        Debug.Log("down:" + Down);
+        Debug.Log("left:" + Left);
+        Debug.Log("right:" + Right);
     }
 }
