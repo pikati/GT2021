e6c3394292e11d5db2e23f42cb405cd873ecde5a
pikatan <pikapisu@gmail.com>
2021-04-26 18:37:26 +0900

    雷エフェクト作り途中

---
+++ Prototype/Assets/Pikatan/Scripts/Effect/ErectricalEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErectricalEffect : MonoBehaviour
+{
+    ErectricalEffectManager eem;
+
+    private void Start()
+    {
+        eem = transform.root.gameObject.transform.GetChild(0).GetComponent<ErectricalEffectManager>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        eem.OnCollisionErecParticle();
+    }
+}
