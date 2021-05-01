126d14aa29846bbd00d390c724a488999a9ee97d
pikatan <pikapisu@gmail.com>
2021-05-01 21:53:06 +0900

    凍る床実装

---
+++ Prototype/Assets/Pikatan/Scripts/Player/PlayerState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public enum PlayerStateEnum
+    { 
+        Move,
+        Slide,
+        Max
+    }
+
+    public PlayerStateEnum state { get; set; } = PlayerStateEnum.Move;
+}
