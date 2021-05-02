418bb015da1a6254dca20d7fa179b016ded8300d
yamapiro36 <yukkuri0408@gmail.com>
2021-05-02 21:38:20 +0900

    スイッチ作る環境整えと少しのスクリプトいじり

---
+++ Prototype/Assets/Yamapiro/Script/Switch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool is_On;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!is_On)
+        {
+            is_On = true;
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
