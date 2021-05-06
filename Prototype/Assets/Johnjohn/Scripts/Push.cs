3134418873a76af1a61fee35ef7ddc84b38d4086
Johnjohn55 <a144243495@icloud.com>
2021-05-06 12:50:30 +0900

    ブロック動いた（仮）

--- Prototype/Assets/Johnjohn/Scripts/Push.cs
+++ Prototype/Assets/Johnjohn/Scripts/Push.cs
@@ -4,40 +4,109 @@ using UnityEngine;
 
 public class Push : MonoBehaviour
 {
-    //BoxCollider用変数
-    private BoxCollider[] boxcol;
+    [SerializeField]
+    private GameObject startObj;
+    [SerializeField]
+    private GameObject endObj;
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private enum PushState
+    {
+        Init,
+        Uninit
+    };
+
+    private enum PushMoveState
+    {
+        Stop,
+        Move
+    };
+
+    private PushState pushState = PushState.Init;
+    private PushMoveState pushMoveState = PushMoveState.Stop;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("開始");
+        Debug.Log("s" + startObj.transform.position);
+        Debug.Log("e" + endObj.transform.position);
 
-        //BoxCollider取得
-        boxcol = GetComponents<BoxCollider>();
-
-        //for (int i = 0; i < col.Length; i++)
-        //{
-        //    col[i].enabled = false;
-        //}
     }
 
+    // Update is called once per frame
     void Update()
     {
-        
 
+        Move();
+        
 
     }
 
-    void OnTriggerStay(Collider other)//接触中判定
+    private void OnCollisionEnter(Collision collision)
     {
-
-        Debug.Log("接触中");
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(collision.gameObject.CompareTag("Player"))
         {
-            this.transform.Translate(1.0f, 0.0f, 0.0f);
+            if(pushMoveState==PushMoveState.Stop)
+            {
+                pushMoveState = PushMoveState.Move;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (pushMoveState == PushMoveState.Stop)
+            return;
+
+        if(pushState==PushState.Uninit)
+        {
+            transform.position = Vector3.Lerp(transform.position, startObj.transform.position, speed * Time.deltaTime);
+            if (IsMoveCompleted(startObj.transform.position))
+                ChengeState();
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, endObj.transform.position, speed * Time.deltaTime);
+            if (IsMoveCompleted(endObj.transform.position))
+                ChengeState();
+        }
+
+        
+    }
+
+    private bool IsMoveCompleted(Vector3 Target)
+    {
+        if (Vec3Abs(transform.position, Target) <= 0.1f)
+            return true;
+        return false;
+
+    }
+
+    private float Vec3Abs(Vector3 a, Vector3 b)
+    {
+        return (Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y) + Mathf.Abs(a.z - b.z)) / 3.0f;
+    }
+
+    private void ChengeState()
+    {
+        if (PushState.Init==pushState)
+        {
+            pushState = PushState.Uninit;
+        }
+        else
+        {
+            pushState = PushState.Init;
+        }
+
+        if (PushMoveState.Stop == pushMoveState)
+        {
+            pushMoveState = PushMoveState.Move;
+        }
+        else
+        {
+            pushMoveState = PushMoveState.Stop;
         }
 
     }
-
 }
