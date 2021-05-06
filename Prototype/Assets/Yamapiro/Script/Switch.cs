f317a20b4074ac327a8f2f00199d37f119b321e8
pikatan <pikapisu@gmail.com>
2021-05-06 11:10:50 +0900

    スイッチ実装、パネルの当たり判定調整、プレイヤープレハブの当たり判定更新

--- Prototype/Assets/Yamapiro/Script/Switch.cs
+++ Prototype/Assets/Yamapiro/Script/Switch.cs
@@ -7,11 +7,11 @@ public class Switch : MonoBehaviour
 
     public GameObject Panel;
 
-    public GameObject[] SwitchObj;
+    private GameObject[] SwitchObj;
 
     private List<Switch> Switches = new List<Switch>();
 
-    public bool is_On;
+    private bool is_On = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +23,6 @@ public class Switch : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-  
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
@@ -38,7 +33,7 @@ public class Switch : MonoBehaviour
                 ChangeSwitchState(true);
                 foreach(Switch sw in Switches)
                 {
-                    sw.ChangeSwitchState(false);
+                    sw.ChangeSwitchState(false, gameObject.name);
                 }
             }
             else
@@ -47,14 +42,16 @@ public class Switch : MonoBehaviour
                 ChangeSwitchState(false);
                 foreach (Switch sw in Switches)
                 {
-                    sw.ChangeSwitchState(true);
+                    sw.ChangeSwitchState(false, gameObject.name);
                 }
             }
         }
     }
 
-    private void ChangeSwitchState(bool is_On)
+    private void ChangeSwitchState(bool is_On, string name = "non")
     {
+        if (name == gameObject.name) return;
+        Debug.Log(gameObject.name + " 通った");
         this.is_On = is_On;
         if (is_On)
         {
