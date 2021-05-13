1285180660bbb7c938f7a0891e19993d9724d7b6
pikatan <pikapisu@gmail.com>
2021-05-13 19:01:36 +0900

    シーンの絵作ったやつ

---
+++ Prototype/Assets/Pikatan/Scripts/Utilitys/NavMeshDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class NavMeshDrawer : Singleton<NavMeshDrawer>
+{
+
+    void Awake()
+    {
+        DrawNwvMesh();
+    }
+
+    public void DrawNwvMesh()
+    {
+        //NavMeshの三角形集合取得
+        NavMeshTriangulation triangles = NavMesh.CalculateTriangulation();
+
+        //三角形集合からMeshを生成
+        Mesh mesh = new Mesh();
+        mesh.vertices = triangles.vertices;
+        mesh.triangles = triangles.indices;
+
+        //MeshFilterに生成したMeshを渡す
+        MeshFilter filter = GetComponent<MeshFilter>();
+        filter.mesh = mesh;
+    }
+}
