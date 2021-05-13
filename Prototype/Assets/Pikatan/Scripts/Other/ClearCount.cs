9d58939da473d53cb41d64c27924fa3dc543714c
pikatan <pikapisu@gmail.com>
2021-05-13 19:39:57 +0900

    UI差し替え、それに伴ってスクリプトも修正

--- Prototype/Assets/Pikatan/Scripts/Other/ClearCount.cs
+++ Prototype/Assets/Pikatan/Scripts/Other/ClearCount.cs
@@ -5,16 +5,25 @@ using UnityEngine.UI;
 
 public class ClearCount : Singleton<ClearCount>
 {
-    private Text num;
+    private List<GameObject> goalImages = new List<GameObject>(5);
+    private int goalCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        num = GetComponent<Text>();
-        num.text = "あと" + Singleton<ClearChecker>.Instance.ClearNum + "チェックポイント";
+        goalImages.Add(transform.GetChild(1).gameObject);
+        goalImages.Add(transform.GetChild(2).gameObject);
+        goalImages.Add(transform.GetChild(3).gameObject);
+        goalImages.Add(transform.GetChild(4).gameObject);
+        goalImages.Add(transform.GetChild(5).gameObject);
+        for(int i = Singleton<ClearChecker>.Instance.ClearNum; i < 5; i++)
+        {
+            goalImages[i].SetActive(false);
+        }
     }
 
     public void UpdateClearNum()
     {
-        num.text = "あと" + Singleton<ClearChecker>.Instance.ClearNum + "チェックポイント";
+        //ゴールの画像オブジェの子供にあるMaskオブジェを取得しそれを非表示に
+        goalImages[goalCount++].transform.GetChild(1).gameObject.SetActive(false);
     }
 }
