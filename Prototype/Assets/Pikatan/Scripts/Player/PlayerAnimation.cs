b7cd9c5f1697b6a0151012e7f80287d5a3e139a6
pikatan <pikapisu@gmail.com>
2021-05-18 16:53:09 +0900

    プレイヤーのモデル差し替え

---
+++ Prototype/Assets/Pikatan/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    public float MoveValue { get; set; }
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Move", MoveValue);
+    }
+}
