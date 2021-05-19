11133bbb3f6c2e85aede626d08540e2b402a7e98
pikatan <pikapisu@gmail.com>
2021-05-20 00:42:39 +0900

    タイトルのボタン選択処理をオレオレボタン処理に変更

---
+++ Prototype/Assets/Pikatan/Scripts/UI/ButtonUIController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonUIController : MonoBehaviour
+{
+    [SerializeField]
+    private int buttonNum;
+    [SerializeField]
+    private UnityEvent[] buttonEvent;
+    private List<GameObject> cursors = new List<GameObject>();
+    private InputController ic;
+    private bool isInput = false;
+    public int ButtonIdx { get; private set; } = 0;
+    void Start()
+    {
+        for(int i = 0; i < buttonNum; i++)
+        {
+            cursors.Add(transform.GetChild(i).gameObject);
+        }
+        for(int i = 1; i < buttonNum; i++)
+        {
+            DisableCursor(i);
+        }
+        ic = Singleton<InputController>.Instance;
+    }
+
+    void Update()
+    {
+        if(ic.MoveValue.y > 0 || ic.ArrowValue.y > 0)
+        {
+            if (isInput) return;
+            ChangeCursor(-1);
+        }
+        else if(ic.MoveValue.y < 0 || ic.ArrowValue.y < 0)
+        {
+            if (isInput) return;
+            ChangeCursor(1);
+        }
+        else
+        {
+            isInput = false;
+        }
+        if(ic.A)
+        {
+            buttonEvent[ButtonIdx].Invoke();
+        }
+    }
+
+    private void ChangeCursor(int n)
+    {
+        DisableCursor(ButtonIdx);
+        ButtonIdx += n;
+        if(ButtonIdx < 0)
+        {
+            ButtonIdx = 0;
+        }
+        if(ButtonIdx >= cursors.Count)
+        {
+            ButtonIdx = cursors.Count - 1;
+        }
+        EnableCursor(ButtonIdx);
+        isInput = true;
+    }
+
+    private void EnableCursor(int index)
+    {
+        cursors[index].SetActive(true);
+        cursors[index].GetComponent<CursorAnimation>().StartAniamtion();
+    }
+
+    private void DisableCursor(int index)
+    {
+        cursors[index].SetActive(false);
+    }
+}
