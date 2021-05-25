866d15a05d17fcdd57f819847d59655e57d74637
Amalgam0616 <56029851+Amalgam0616@users.noreply.github.com>
2021-05-25 17:59:57 +0900

    ステージ画像スクリプティング

---
+++ Prototype/Assets/Amalgam/Script/PutStageImage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PutStageImage : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] StagePhotos;
+
+    [SerializeField]
+    private Image StageImage;
+
+    [SerializeField]
+    public int Index { get; set; }
+
+    private int OldIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StageImage = GetComponent<Image>();
+        Index = OldIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OldIndex != Index)
+        {
+            if(Index > StagePhotos.Length)
+            {
+                Index = OldIndex;
+            }
+
+            StageImage.sprite = StagePhotos[Index];
+            OldIndex = Index;
+        }
+    }
+}
