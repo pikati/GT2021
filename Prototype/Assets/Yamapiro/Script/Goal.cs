5e9ce32f25bd8f0204682ae25c49ecbef907fd94
Amalgam0616 <56029851+Amalgam0616@users.noreply.github.com>
2021-06-21 19:22:21 +0900

    ゴールに演出追加

--- Prototype/Assets/Yamapiro/Script/Goal.cs
+++ Prototype/Assets/Yamapiro/Script/Goal.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    //演出状態
+    enum MEMORY_STATE
+    {
+        WAIT = 0,
+        SPIN,
+        END,
+    };
+
+    private MEMORY_STATE memoryState;
+
     private bool isReach = false;
     private ClearCount clearCount;
     [SerializeField]
@@ -13,6 +23,8 @@ public class Goal : MonoBehaviour
     private GoalChild goalChild1;
     private GoalChild goalChild2;
 
+    private GameTimer spinFrameCnt;
+
     private void Start()
     {
         emitter = transform.GetChild(1).gameObject;
@@ -20,15 +32,37 @@ public class Goal : MonoBehaviour
         memory = transform.Find("memori-kai").gameObject;
         goalChild1 = transform.Find("Col1").GetComponent<GoalChild>();
         goalChild2 = transform.Find("Col2").GetComponent<GoalChild>();
+
+        memoryState = MEMORY_STATE.WAIT;
+        spinFrameCnt = new GameTimer(1.5f);
     }
     private void Update()
     {
-
-        memory.transform.Rotate(60 * Time.deltaTime, 120 * Time.deltaTime, 30 * Time.deltaTime);
-        clearCount = GameObject.Find("GoalUI").GetComponent<ClearCount>();
-        if(goalChild1.IsGoal && goalChild2.IsGoal)
+        //待機
+        if (memoryState == MEMORY_STATE.WAIT)
         {
-            GoalFnac();
+            memory.transform.Rotate(60 * Time.deltaTime, 120 * Time.deltaTime, 30 * Time.deltaTime);
+        }
+        //回転
+        else if(memoryState == MEMORY_STATE.SPIN)
+        {
+            if(!spinFrameCnt.IsTimeUp)
+            {
+                memory.transform.Rotate(0.0f, 100000.0f * Time.deltaTime, 0.0f);
+                memory.transform.position += new Vector3(0.0f, 0.4f, 0.0f);
+            }
+            else
+            {
+                ChangeState(MEMORY_STATE.END);
+            }
+
+            spinFrameCnt.UpdateTimer();
+        }
+
+        clearCount = GameObject.Find("GoalUI").GetComponent<ClearCount>();
+        if(goalChild1.IsGoal && goalChild2.IsGoal && memoryState == MEMORY_STATE.WAIT)
+        {
+            ChangeState(MEMORY_STATE.SPIN);
         }
     }
 
@@ -36,24 +70,43 @@ public class Goal : MonoBehaviour
     {
         if (isReach) return;
         if (isGoal) return;
-        if(other.CompareTag("Player"))
+
+        if(other.CompareTag("Player") && memoryState == MEMORY_STATE.WAIT)
         {
-            GoalFnac();
+            ChangeState(MEMORY_STATE.SPIN);
         }
     }
     private void GoalFnac()
     {
         isReach = true;
-        Singleton<ClearChecker>.Instance.ReachChechkPoint();
+        Singleton<ClearChecker>.Instance.ReachChechkPoint();    //ここでUI出してる
         //常時出し続ける仕様変更のため
         //clearCount.ChangeState(ClearCount.ImageState.Visible);
         transform.GetChild(0).gameObject.SetActive(false);
-        emitter.SetActive(true);
         goalChild1.gameObject.SetActive(false);
         goalChild2.gameObject.SetActive(false);
-        Singleton<SoundManager>.Instance.PlaySeByName("get");
-        Destroy(gameObject, 1.5f);
-        isGoal = true;
+        Destroy(gameObject);
         Singleton<NavMeshBaker>.Instance.Bake();
+    }
+
+    private void ChangeState(MEMORY_STATE next)
+    {
+        if(next == MEMORY_STATE.WAIT)
+        {
+            Debug.Log("ここにきちゃだめー！(幼女)");
+        }
+        else if (next == MEMORY_STATE.SPIN)
+        {
+            memoryState = MEMORY_STATE.SPIN;
+            emitter.SetActive(true);
+            Singleton<SoundManager>.Instance.PlaySeByName("get");
+            isGoal = true;
+            memory.transform.Rotate(0.0f, 0.0f, 0.0f);
+        }
+        else if (next == MEMORY_STATE.END)
+        {
+            memoryState = MEMORY_STATE.END;
+            GoalFnac();
+        }
     }
 }
