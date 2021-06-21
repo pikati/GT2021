9ee030b3aa0ec98288d0b8c44087bfd880522acd
pikatan <pikapisu@gmail.com>
2021-06-21 17:15:16 +0900

    ゴールの当たり判定変更中（モデル修正待ち）

--- Prototype/Assets/Yamapiro/Script/Goal.cs
+++ Prototype/Assets/Yamapiro/Script/Goal.cs
@@ -10,18 +10,26 @@ public class Goal : MonoBehaviour
     private GameObject emitter;
     private GameObject memory;
     private bool isGoal = false;
+    private GoalChild goalChild1;
+    private GoalChild goalChild2;
 
     private void Start()
     {
         emitter = transform.GetChild(1).gameObject;
         emitter.SetActive(false);
         memory = transform.Find("memori-kai").gameObject;
+        goalChild1 = transform.Find("Col1").GetComponent<GoalChild>();
+        goalChild2 = transform.Find("Col2").GetComponent<GoalChild>();
     }
     private void Update()
     {
 
         memory.transform.Rotate(60 * Time.deltaTime, 120 * Time.deltaTime, 30 * Time.deltaTime);
         clearCount = GameObject.Find("GoalUI").GetComponent<ClearCount>();
+        if(goalChild1.IsGoal && goalChild2.IsGoal)
+        {
+            GoalFnac();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,16 +38,22 @@ public class Goal : MonoBehaviour
         if (isGoal) return;
         if(other.CompareTag("Player"))
         {
-            isReach = true;
-            Singleton<ClearChecker>.Instance.ReachChechkPoint();
-            //常時出し続ける仕様変更のため
-            //clearCount.ChangeState(ClearCount.ImageState.Visible);
-            transform.GetChild(0).gameObject.SetActive(false);
-            emitter.SetActive(true);
-            Singleton<SoundManager>.Instance.PlaySeByName("get");
-            Destroy(gameObject, 1.5f);
-            isGoal = true;
-            Singleton<NavMeshBaker>.Instance.Bake();
+            GoalFnac();
         }
+    }
+    private void GoalFnac()
+    {
+        isReach = true;
+        Singleton<ClearChecker>.Instance.ReachChechkPoint();
+        //常時出し続ける仕様変更のため
+        //clearCount.ChangeState(ClearCount.ImageState.Visible);
+        transform.GetChild(0).gameObject.SetActive(false);
+        emitter.SetActive(true);
+        goalChild1.gameObject.SetActive(false);
+        goalChild2.gameObject.SetActive(false);
+        Singleton<SoundManager>.Instance.PlaySeByName("get");
+        Destroy(gameObject, 1.5f);
+        isGoal = true;
+        Singleton<NavMeshBaker>.Instance.Bake();
     }
 }
