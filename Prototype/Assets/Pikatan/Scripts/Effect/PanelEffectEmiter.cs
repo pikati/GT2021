1933c654fb0494ab010ce3b336c20d7ad68eecc4
pikatan <pikapisu@gmail.com>
2021-06-28 19:28:40 +0900

    実行ファイル生成、軸のエフェクト追加中

---
+++ Prototype/Assets/Pikatan/Scripts/Effect/PanelEffectEmiter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelEffectEmiter : MonoBehaviour
+{
+    private GameObject effect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effect = Resources.Load("SelectEffect") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
