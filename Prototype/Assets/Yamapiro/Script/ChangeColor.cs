7c8c3ef971dc6daf195aa8198fd91569e71dac59
pikatan <pikapisu@gmail.com>
2021-07-05 18:59:57 +0900

    軸を点滅するように変更

--- Prototype/Assets/Yamapiro/Script/ChangeColor.cs
+++ Prototype/Assets/Yamapiro/Script/ChangeColor.cs
@@ -11,12 +11,21 @@ public class ChangeColor : MonoBehaviour
         OnPlayer
     };
 
+    private enum DefaultState 
+    { 
+        ToWhite,
+        ToYellow
+    }
+
+
     public AxisState AState { get; set; } = AxisState.Non;
     private bool isFocus = false;
     private Renderer defaultRenderer;
     private Material defaultMat;
     private Material changeMat;
     private Material focusMat;
+    private DefaultState DState = DefaultState.ToWhite;
+    private GameTimer defaultColorTimer = new GameTimer(1.0f);
     //[SerializeField]
     //private GameObject a;
     
@@ -38,6 +47,7 @@ public class ChangeColor : MonoBehaviour
                 defaultRenderer.material = changeMat;
                 break;
             case AxisState.Non:
+                ChangeDefaultColor();
                 defaultRenderer.material = defaultMat;
                 break;
             case AxisState.Forcus:
@@ -77,6 +87,35 @@ public class ChangeColor : MonoBehaviour
             ChangeAxisState(AxisState.Non);
         }
         //a.SetActive(isFocus);
+    }
+
+    private void ChangeDefaultColor()
+    {
+        if(defaultColorTimer.UpdateTimer())
+        {
+            if(DState == DefaultState.ToWhite)
+            {
+                DState = DefaultState.ToYellow;
+            }
+            else
+            {
+                DState = DefaultState.ToWhite;
+            }
+            defaultColorTimer.ResetTimer(1.0f);
+        }
+        switch (DState)
+        {
+            case DefaultState.ToWhite:
+                defaultMat.color = Color.Lerp(defaultMat.color, Color.white, Time.deltaTime * 2);
+                break;
+            case DefaultState.ToYellow:
+                defaultMat.color = Color.Lerp(defaultMat.color, Color.yellow, Time.deltaTime * 2);
+                break;
+            default:
+                break;
+        }
+        
+
     }
 
     public void ChangeAxisState(AxisState state)
