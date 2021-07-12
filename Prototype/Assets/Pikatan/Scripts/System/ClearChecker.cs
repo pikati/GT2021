0aa737d264f46aa2f73656be437340abc8e2266c
pikatan <pikapisu@gmail.com>
2021-07-12 17:46:03 +0900

    クリア時に花火エフェクト追加

--- Prototype/Assets/Pikatan/Scripts/System/ClearChecker.cs
+++ Prototype/Assets/Pikatan/Scripts/System/ClearChecker.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ClearChecker : Singleton<ClearChecker>
 {
+    private GameObject obj;
+    private GameObject effect;
     public int checkPointCount = 1;
     public int ClearNum => checkPointCount;
     public bool IsClear { get; private set; } = false;
@@ -12,20 +14,22 @@ public class ClearChecker : Singleton<ClearChecker>
     {
         GameObject[] goalObjs = GameObject.FindGameObjectsWithTag("Goal");
         checkPointCount = goalObjs.Length;
+        obj = Resources.Load("Hanabi_Effect") as GameObject;
     }
 
     public void ReachChechkPoint()
     {
         checkPointCount--;
         Singleton<ClearCount>.Instance.UpdateClearNum();
-        if(checkPointCount <= 0)
+        if(checkPointCount <= 0 && !IsClear)
         {
             Singleton<SoundManager>.Instance.StopBgm();
             Singleton<SoundManager>.Instance.PlayBgmByName("result");
-            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>().ClearRotation();
+            var p = GameObject.FindGameObjectWithTag("Player");
+            p.GetComponent<PlayerMove>().ClearRotation();
             IsClear = true;
             Singleton<GameManager>.Instance.ChangeGameState(GameManager.GameState.Clear);
-
+            effect = Instantiate(obj, p.transform.position + new Vector3(0, 5, 5), Quaternion.identity);
         }
     }
 }
